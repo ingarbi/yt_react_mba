7b1acedc27a4693a09e4352329a6ac65259bbd12
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-29 20:51:06 +0300

    #2 structure

--- my_bank_app/src/App.jsx
+++ my_bank_app/src/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import styles from "./style"
+
+import {Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero} from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <CTA/>
+          <Testimonials/>
+          <Clients/>
+          <Footer/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
