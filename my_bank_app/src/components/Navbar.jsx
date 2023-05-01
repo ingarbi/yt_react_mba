4b0ae35f47bb589f16524cd6ebded8ac85a576a4
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-01 11:48:04 +0300

    #5 Stats Component

--- my_bank_app/src/components/Navbar.jsx
+++ my_bank_app/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center">
+    <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
