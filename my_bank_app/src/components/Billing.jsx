be010a0f5053731a20cd51e92085c9e48403069d
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-02 23:41:10 +0300

    #7 Billing

--- my_bank_app/src/components/Billing.jsx
+++ my_bank_app/src/components/Billing.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
+const Billing = () => (
+    <section className={layout.sectionReverse} id='product'>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="billing"  className='w-[100%] h-[100%] relative z-[5]'/>
+        
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] rounded-full white__gradient' />
+        
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] rounded-full pink__gradient' />
+      </div>
+      
+      <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
+          <p className={`${styles.paragraph} max-w-470px mt-5`}>
+          From its medieval origins to the digital era, learn everything there
+          is to know about the ubiquitous lorem ipsum passage.
+          </p>
+
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+          <img src={apple} alt="appleStore" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+          <img src={google} alt="googlePlay" className='w-[128px] h-[42px] object-contain cursor-pointer' />
+        </div>
+
+      </div>
+
+      
+
+    </section>
   )
-}
 
 export default Billing
