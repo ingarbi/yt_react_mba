10926a97df2527d4e6bc4965c99030cce2e54e80
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-03 20:10:01 +0300

    #8 Card

--- my_bank_app/src/components/CardDeal.jsx
+++ my_bank_app/src/components/CardDeal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles, { layout } from "../style";
+import { card } from "../assets";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in a few
+        steps
+      </h2>
+      <p className={`${styles.paragraph} max-w[470px] mt-5`}>
+        From its medieval origins to the digital era, learn everything there is
+        to know about the ubiquitous lorem ipsum passage.
+      </p>
+      <Button styleTop="mt-10" />
+    </div>
 
-export default CardDeal
+    <div>
+      <img src={card} alt="cardInfo" className="w-[100%] h-[100%]" />
+    </div>
+
+  </section>
+);
+
+export default CardDeal;
